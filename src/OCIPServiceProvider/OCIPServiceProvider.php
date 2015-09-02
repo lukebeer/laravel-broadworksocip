41e91e1e0bbb471cfb8123fc3a519a281fd1c340
lukebeer <mail@luke.beer>
2015-09-02 21:14:54 +0100

    Created composer package

---
+++ src/OCIPServiceProvider/OCIPServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Laravel BroadworksOCIP Package https://github.com/lukebeer/laravel-broadworksocip
+ *
+ * Copyright (c) 2015 Luke Berezynskyj (aka Luke Beer)
+ *
+ * @author Luke Berezynskyj <mail@luke.beer>
+ */
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OCIPServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
